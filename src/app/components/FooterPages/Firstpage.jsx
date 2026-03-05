d6c345068eb6683ec086a01b4e32fa96e3b20705
Himanshu87504 <165474672+Himanshu87504@users.noreply.github.com>
2026-03-05 11:03:40 +0530

    updated

--- src/app/components/FooterPages/Firstpage.jsx
+++ src/app/components/FooterPages/Firstpage.jsx
@@ -1,29 +1,37 @@
+"use client";
 import React from "react";
 import Image from "next/image";
-import Ellipse from '../image/Ellipse.png'
-import Groupcircular from '../image/Groupcircular.png'
-
+import { motion } from "framer-motion";
+import Ellipse from '../image/Ellipse.png';
+import Groupcircular from '../image/Groupcircular.png';
 
 const FirstPage = () => {
     return (
-        <div className="min-h-screen bg-[#ffce31] font-sans overflow-hidden relative selection:bg-black selection:text-white flex">
-
-            <div>
+        <div className="h-screen bg-[#ffce31] font-sans overflow-hidden relative selection:bg-black selection:text-white flex flex-col items-start justify-start ">
+            <div className="mb-4">
                 <Image
                     src={Ellipse}
+                    alt="Ellipse"
                     width={205}
                     height={20}
                 />
             </div>
-            <div>  i want thies image rotate circular to the first image add the animaatinon
-                <div className="relative top-70 right-25">
+            <div className="relative -top-140 left-26"> {/* Curve position: up-left from Groupcircular */}
+                <motion.div
+                    className="relative"
+                    animate={{ rotate: 360 }}
+                    transition={{ duration: 10, repeat: Infinity, ease: "linear" }}
+                >
                     <Image
                         src={Groupcircular}
+                        alt="Group circular"
                         width={200}
                         height={200}
                     />
-                </div>
+                </motion.div>
             </div>
+
+
         </div>
     );
 };
